d3154d0b06fcf6d907305209724e2c56b1f6bacc
“Gunjal-Akash” <akash.gunjal@500x.tech>
2023-03-16 17:51:19 +0530

    add fierst line code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('thius is test')
 })
 let port = process.env.port || 3000
 app.listen(port, () => {
